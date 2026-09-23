66db597e2d6924b2912931c8665eb3eb40b7a031
Orlando <orlando@Orlandos-MacBook-Pro.local>
2026-09-23 10:04:54 -0400

    initial commit

---
+++ fastapi-proj/main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI, Query
+from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
+
+STATUS_CODES = {
+    100: "PENDING",
+    200: "SUCCESS",
+    400: "BAD_REQUEST",
+    500: "SERVER_ERROR",
+}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
+# GETS
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/status/{status_id}")
+def status(status_id: int):
+    return("oh no, status code is: " + STATUS_CODES.get(status_id, "SIGERROR"))
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# POSTS 
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.post("/items_alpha/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# PUT
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
+
+@app.put("/items_alpha/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
